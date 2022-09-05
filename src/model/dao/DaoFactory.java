0bbbdd882f7b4d9c8c4994824f574100aa0ffe62
Yasmin Hikari <yhaoyama@gmail.com>
2022-09-05 14:46:45 -0300

    Adding database access

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,17 @@
+package model.dao;
+
+import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+	/* Cria injeção de dependencia - esconde a implementação do SellerDao*/
+	public static SellerDao createSellerDao() {
+		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
+}
